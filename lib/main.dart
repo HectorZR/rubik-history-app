094895c7da3150a4e02bf7e1e2e27b875b3ff80c
Hector Zurga <hjzurga@gmail.com>
2023-05-28 19:05:02 -0400

    Lock orientation to vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:mobile_app/screens/home/Home.dart';
 
 void main() {
-  runApp(const RubiksTimerApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) => runApp(const RubiksTimerApp()));
 }
 
 class RubiksTimerApp extends StatelessWidget {
@@ -14,22 +18,7 @@ class RubiksTimerApp extends StatelessWidget {
     return MaterialApp(
       title: 'Rubiks Cube Timer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
         useMaterial3: true,
       ),
       home: const Home(),
